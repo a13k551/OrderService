1224a19cef38312ba602c618d25652267c0db6f9
a13k55 <a13k55@gmial.com>
2023-08-28 22:05:00 +0300

    feat: add config

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,22 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"net"
 	"net/http"
 	"time"
 
+	"github.com/a13k551/OrderService/internal/config"
 	"github.com/a13k551/OrderService/internal/order"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 
+	conf := config.Get()
+
 	router := mux.NewRouter()
 	router.StrictSlash(true)
 
 	order.AddHandlers(router)
 
-	listener, err := net.Listen("tcp", "localhost:5000")
+	address := fmt.Sprintf("%s:%s", conf.App.BindIP, conf.App.Port)
+	listener, err := net.Listen("tcp", address)
 
 	if err != nil {
 		panic(err)
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,40 @@
 package config
+
+import (
+	"log"
+	"sync"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	IsDebug *bool `yaml:"is_debug"`
+	App     struct {
+		BindIP string `yaml:"bind_ip"`
+		Port   string `yaml:"port"`
+	} `yaml:"app"`
+	Postgree struct {
+		Host     string `yaml:"host"`
+		Port     string `yaml:"port"`
+		Database string `yaml:"database"`
+		Username string `yaml:"username"`
+		Password string `yaml:"password"`
+	} `yaml:"postgree"`
+}
+
+var instance *Config
+var once sync.Once
+
+func Get() *Config {
+	once.Do(func() {
+		instance = &Config{}
+
+		if err := cleanenv.ReadConfig("../../config.yml", instance); err != nil {
+			help, _ := cleanenv.GetDescription(instance, nil)
+			log.Println(help)
+			log.Fatal(err)
+		}
+	})
+
+	return instance
+}
